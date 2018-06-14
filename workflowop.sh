472673c8783b6f5f17e6c6608fdc0b7f78b1bdd3
Bryan Larsen <bryan@larsen.st>
2018-06-14 17:41:57 -0400

    cache json fragment for massive speedup on large files

--- workflowop.sh
+++ workflowop.sh
@@ -8,6 +8,9 @@ KUBECTL="kubectl ${KUBECTL_ARGS}"
 SPEC=${SPEC:-spec/spec.json}
 LOOP_DELAY=${LOOP_DELAY:-15}
 
+
+length=$(jq -r 'length' ${SPEC})
+
 while true ; do
     all_complete=true
     total=0
@@ -16,12 +19,13 @@ while true ; do
     have_pending=0
     have_started=0
 
-    for i in $(jq -r 'to_entries[].key' ${SPEC}) ; do
+    for i in `seq 0 $(($length - 1))` ; do
         #echo "Checking #${i} $(jq -r .[${i}].selector ${SPEC})"
 
+        fragment=$(jq -r ".[${i}]" ${SPEC})
         let total+=1
         outputs_exist=true
-        for output_path in $(jq -r ".[${i}].outputs[].path" ${SPEC}); do
+        for output_path in $(echo $fragment | jq -r ".outputs[].path"); do
             if stat $output_path 2>/dev/null > /dev/null; then
                 true
                 #echo "output $output_path exists"
@@ -33,7 +37,7 @@ while true ; do
         done
 
         if [ $outputs_exist = true ] ; then
-            echo "All outputs exist: " $(jq -r ".[${i}].outputs[].path" ${SPEC})
+            echo "All outputs exist: " $(echo $fragment | jq -r ".outputs[].path")
             let have_outputs+=1
             continue
         else
@@ -41,7 +45,7 @@ while true ; do
         fi
 
         inputs_exist=true
-        for input_path in $(jq -r ".[${i}].inputs[].path" ${SPEC}); do
+        for input_path in $(echo $fragment | jq -r ".inputs[].path"); do
             if stat $input_path 2>/dev/null > /dev/null; then
                 true
                 #echo "input $input_path exists"
@@ -57,11 +61,11 @@ while true ; do
             continue
         fi
 
-        echo "All inputs exist: " $(jq -r ".[${i}].inputs[].path" ${SPEC})
+        echo "All inputs exist: " $(echo $fragment | jq -r ".inputs[].path")
         let have_inputs+=1
 
         # possible statuses: Completed ContainerCreating Error Pending Running Unknown Succeeded Failed
-        pod_statuses=$(${KUBECTL} get pods --selector=$(jq -r .[${i}].selector ${SPEC}) --no-headers 2>/dev/null | tr -s ' ' | cut -d ' ' -f 3)
+        pod_statuses=$(${KUBECTL} get pods --selector=$(echo $fragment | jq -r .selector) --no-headers 2>/dev/null | tr -s ' ' | cut -d ' ' -f 3)
         echo "#${i} pod statuses" ${pod_statuses}
 
         if echo $pod_statuses | grep 'Pending\|ContainerCreating\|Running\|CrashLoopBackoff' ; then
@@ -71,7 +75,7 @@ while true ; do
         fi
 
         let have_started+=1
-        echo $(jq -r .[${i}].spec ${SPEC}) | ${KUBECTL} create -f -
+        echo $(echo $fragment | jq -r .spec) | ${KUBECTL} create -f -
 
     done
     if [ $all_complete = true ] ; then
@@ -79,8 +83,8 @@ while true ; do
         break
     fi
 
-    sleep ${LOOP_DELAY}
     echo "Complete: ${have_outputs}/${total}; Ready: ${have_inputs}, Pending: ${have_pending}, Started: ${have_started}"
+    sleep ${LOOP_DELAY}
 done
 
 exit 0
