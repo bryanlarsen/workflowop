d7fa77ab4c7ac311c76dabb63fb91956b2c08c89
Bryan Larsen <bryan@larsen.st>
2018-06-15 09:52:13 -0400

    do stat in single command rather than in a loop for significant speedup, I hope

--- workflowop.sh
+++ workflowop.sh
@@ -10,6 +10,7 @@ LOOP_DELAY=${LOOP_DELAY:-15}
 
 echo "Starting"
 readarray -t specs < <(jq -c '.[]' ${SPEC})
+echo "Spec read."
 
 while true ; do
     all_complete=true
@@ -23,60 +24,38 @@ while true ; do
         #echo "Checking #${i} $(jq -r .[${i}].selector ${SPEC})"
 
         let total+=1
-        outputs_exist=true
-        for output_path in $(echo $fragment | jq -r ".outputs[].path"); do
-            if stat $output_path 2>/dev/null > /dev/null; then
-                true
-                #echo "output $output_path exists"
-            else
-                outputs_exist=false
-                echo "output $output_path missing"
-                break
-            fi
-        done
+        selector=$(echo $fragment | jq -r .selector)
 
-        if [ $outputs_exist = true ] ; then
-            echo "All outputs exist: " $(echo $fragment | jq -r ".outputs[].path")
+        if stat -t $(echo $fragment | jq -r ".outputs[].path") 2>/dev/null > /dev/null; then
             let have_outputs+=1
+            echo "${selector}: All outputs exist"
             continue
         else
             all_complete=false
         fi
 
-        inputs_exist=true
-        for input_path in $(echo $fragment | jq -r ".inputs[].path"); do
-            if stat $input_path 2>/dev/null > /dev/null; then
-                true
-                #echo "input $input_path exists"
-            else
-                inputs_exist=false
-                echo "input $input_path missing"
-                break
-            fi
-        done
-
-        if [ $inputs_exist = false ] ; then
-            #echo "Some inputs missing, job #${i} waiting for reqs"
+        if stat -t $(echo $fragment | jq -r ".inputs[].path") 2>/dev/null > /dev/null; then
+            true
+        else
+            echo "${selector}: Some inputs missing"
             continue
         fi
 
-        echo "All inputs exist: " $(echo $fragment | jq -r ".inputs[].path")
         let have_inputs+=1
 
         # possible statuses: Completed ContainerCreating Error Pending Running Unknown Succeeded Failed
-        pod_statuses=$(${KUBECTL} get pods --selector=$(echo $fragment | jq -r .selector) --no-headers 2>/dev/null | tr -s ' ' | cut -d ' ' -f 3)
-        echo "#${i} pod statuses" ${pod_statuses}
+        pod_statuses=$(${KUBECTL} get pods --selector=$selector --no-headers 2>/dev/null | tr -s ' ' | cut -d ' ' -f 3)
 
         if echo $pod_statuses | grep 'Pending\|ContainerCreating\|Running\|CrashLoopBackoff' ; then
-            #echo "Job #${i} already started, waiting for it to finish"
+            echo ${selector} : ${pod_statuses}
             let have_pending+=1
             continue
         fi
 
         let have_started+=1
         echo $(echo $fragment | jq -r .spec) | ${KUBECTL} create -f -
-
     done
+
     if [ $all_complete = true ] ; then
         echo "We're all done"
         break
